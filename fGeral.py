9524a7e44267252dd779c5ac78c385e41f4d88ee
LaianaRios <57690299+LaianaRios@users.noreply.github.com>
2019-12-14 16:17:44 -0300

    Add files via upload

---
+++ fGeral.py
@@ -0,0 +1,194 @@
+# ------------ Trabalho de Redes Digitais e Industriais ------------
+# ---------------- Professor: Diego Stéfano (Dinho) ----------------
+# ----------------- Atividade: Formação de Quadros -----------------
+# ----------------- Protocolo: CAN 2.0A - STANDART -----------------
+# --------- Equipe: Laiana Rios, Robson Barbosa, Samuel Dias -------
+# -------------------- Código com Funções Gerais -------------------
+
+# Motivo do fGeral = deixar os códigos do transmissor e receptor mais limpos porque eles usam algumas funções iguais
+
+# Importa o Socket para poder se comunicar com o servidor e obter uma resposta de volta
+import socket
+
+# Função para realizar o cálculo do XOR
+def xor(data, crc): 
+   
+    # Inicializa resultado
+    result = []
+
+   # Atravessa todos os bits, se os bits forem iguais, então XOR é 0, caso contrário 1
+    for i in range(1, len(crc)):
+        if data[i] == crc[i]: 
+            result.append('0') 
+        else: 
+            result.append('1') 
+   
+    return ''.join(result) 
+   
+# A função mod2div faz um XOR entre o divisor e dividendo
+# Realiza a junção dos elementos após passar pela função XOR
+def mod2div(divident, divisor):
+    # Número de bits a serem XORed por vez.
+    pick = len(divisor)
+   
+    tmp = divident[0 : pick]
+   
+    while pick < len(divident): 
+   
+        if tmp[0] == '1': 
+   
+            # Substitui o dividendo pelo resultado do XOR e puxa 1 bit para baixo
+            tmp = xor(divisor, tmp) + divident[pick] 
+   
+        else:
+         
+            # Se o bit mais a esquerda for '0'
+            # Se o bit mais à esquerda do dividendo (ou a parte usada em cada etapa) for 0, a etapa não poderá
+            # usar o divisor regular; precisamos usar um divisor de todos os 0s.
+            tmp = xor('0'*pick, tmp) + divident[pick]
+   
+        pick += 1
+
+   # Nos últimos n bits, precisamos executá-lo normalmente, pois o aumento do valor de pick causará o Índice fora dos limites.
+    if tmp[0] == '1':
+        tmp = xor(divisor, tmp) 
+    else: 
+        tmp = xor('0'*pick, tmp) 
+   
+    checkword = tmp 
+    return checkword
+
+# Função usada no lado do remetente para codificar dados, acrescentando o restante da divisão modular no final dos dados.
+# encodeData = coloca os zeros necessários após o dado enviado (número da chave - 1)
+# Manda essa informação para o mod2div (que faz a divisão exclusiva) e retorna para o transmissor
+def encodeData(msg, key):
+   
+     l_key = len(key)
+   
+    # Adiciona n-1 de "0" no final da mensagem
+     appended_data = msg + '0'*(l_key-1)
+     remainder = mod2div(appended_data, key)
+
+    # Anexar o restante nos dados originais
+     codeword = msg + remainder
+
+     return codeword
+
+# decodeData = coloca os zeros necessários após o dado enviado (número da chave - 1)
+def decodeData(data, key):
+     l_key = len(key)
+
+     # Anexa n-1 zeros no final dos dados
+     appended_data = data + '0'*(l_key-1)
+     remainder = mod2div(appended_data, key)
+   
+     return remainder
+
+def crc(bits, n, divisor):
+    dividendo = bits.copy()
+    dividendo.extend([0] * n)
+
+    pos_inicial = dividendo.index(1)
+    pos_final = pos_inicial + (n + 1)
+
+    novo_dividendo = xor(dividendo[pos_inicial : pos_final], divisor)
+
+    fim = False
+
+    while (not fim):
+
+        pos_inicial = novo_dividendo.index(1)
+
+        pedaco_novo_dividendo = novo_dividendo[novo_dividendo.index(1) :]
+        pedaco_antigo_dividendo = dividendo[pos_final:]
+
+        novo_dividendo = xor(pedaco_novo_dividendo + pedaco_antigo_dividendo, divisor)
+
+        pos_final = pos_inicial + (n + 1)
+
+        if pos_final >= len(dividendo):
+            fim = True
+
+# paridadesIN = recebe os dados e os converte em uma lista de ints, printa a lista, determina a posição e o valor do bit que precisa ser inserido
+def paridadesIN(entrada):
+   
+    par = list(map(int, str(entrada)))
+
+    #print("PARIDADES:",par)
+
+    posicoes=[]
+    elementos=[]
+
+    # Calcula onde o bit precisa ser inserido
+    for idx in range(len(par)):
+
+        if idx >= 5:
+            s = sum(par[idx - 5:idx])
+
+            if s == 0:
+                if par[idx] == 0:
+                    posicoes.append(idx)
+                    elementos.append(1)
+            elif s == 5:
+                if par[idx] == 1:
+                    posicoes.append(idx)
+                    elementos.append(0)
+
+    #print("POSIÇÕES ONDE DEVE INSERIR: {}".format(posicoes))
+    #print("ELEMENTOS A SEREM INSERIDOS: {}".format(elementos))
+
+    saida = par
+    #print(saida)
+    # Coloca o bit extra no lugar
+    for x in range(len(par)):
+        if x in posicoes:
+            #print(par[0:x+1])
+            #print([elementos[posicoes.index(x)]])
+            #print(par[x:len(par)-1])
+            saida = par[0:x] + [elementos[posicoes.index(x)]] + par[x:len(par)]
+            #print(saida)
+
+    return "".join([str(elem) for elem in saida])
+
+# paridadesOFF = recebe os dados e os converte em uma lista de ints, printa a lista, determina a posição e o valor do bit que precisa ser retirado
+def paridadesOFF(entrada):
+   
+    #print(entrada)
+    par = list(map(int, str(entrada)))
+
+    #print("PARIDADES:",par)
+
+    posicoes=[]
+    elementos=[]
+
+   # Calcula onde o bit precisa ser retirado
+    for idx in range(len(par)):
+
+        if idx >= 5:
+            s = sum(par[idx - 4:idx])
+
+            if s == 0:
+                if par[idx] == 0:
+                    posicoes.append(idx)
+                    elementos.append(1)
+            elif s == 4:
+                if par[idx] == 1:
+                    posicoes.append(idx)
+                    elementos.append(0)
+
+    #print("POSIÇÕES ONDE DEVE INSERIR: {}".format(posicoes))
+    #print("ELEMENTOS A SEREM INSERIDOS: {}".format(elementos))
+
+    saida = par
+
+    # Retira o bit inserido
+    for x in posicoes:
+        saida[x+1] = "BIT_DE_SAIDA"
+
+    #print(saida)
+    if "BIT_DE_SAIDA" in saida:
+        saida.remove("BIT_DE_SAIDA")
+
+    return "".join([str(elem) for elem in saida])
+
+#dprint(paridadesOFF(paridadesIN("110100000101110")))
---
+++ myhdl_rs232.py
@@ -0,0 +1,255 @@
+# Simulação de uma comunicação entre dois nós utilizando comunicação serial.
+# A simulação consiste de um mestre enviando um pacote de 10 bits (7 de dados,
+# 1 de paridade, 1 de start e 1 de stop) a um escravo.
+# O pacote é pré-codificado dentro do bloco mestre.
+# No escravo ele é armazenado dentro de uma lista, bit a bit, à medida que é recebido.
+
+from myhdl import block, delay, always, now, Signal
+
+from fGeral import *
+
+# Bloco Escravo: define estrutura do bloco que receberá o pacote do mestre.
+# recebe dois sinais:
+# "clock": necessário para a simulaçao funcionar.
+# "dados": onde o escravo vai ler os bits enviados pelo mestre.
+@block
+def Escravo(clock, dados):
+    # Este bloco contém propriedades, variáveis que gardam informações sobre o bloco.
+    # As propriedades podem ser alteráveis ou constantes.
+    # Foram aplicados a estrutura de dado dicionário de Python para representar os estados.
+    
+    # constantes: dicionário que contém estados que não mudam ao longo da execução.
+    constantes = {
+        'tamanho_quadro': 127
+    }
+
+    # estado: dicionário que contém estados que podem mudar durante a execução.
+    estado = {
+        'barramento_inativo': True, # <--- item do dicionário que descreve o estado do barramento.
+        'contagem_bits': 0,          # <--- item do dicionário que descreve quantos bitas foram recebidos.
+        
+        'SOF':'', #BIT[0]
+        'ARBITRATION':'',#BIT[1:11]
+        'CONTROL':'',#BIT[12:17]
+        
+
+        'DATA':'',#BIT[18:-25]*
+        #CRC len() = 16
+        'CRC':'',#BIT[-25:-9]*
+
+        'ACK':'',#BIT[-9:-7]
+        'EOF':'',#BIT[-7:]
+    }
+
+
+    # quadro: lista onde serão armazenados os bits recebidos; o tamanho do quadro está armazenado no
+    #         item "tamanho_quadro" do dicionário "constantes", e é acessado com a linha: constantes['tamanho_quadro']
+    quadro = [0] * constantes['tamanho_quadro']
+    
+    
+    # função "leitura": é chamada a cada subida do clock;
+    # o seu funcionamento depende do status do barramento:
+    #
+    #    se o barramento estiver ativo então:
+    #        se a qtd de bits recebida é igual ao tamanho do quadro (ou seja, se a trasnmissão acabou) então:
+    #            faz o barramento inativo novamente
+    #            reseta a contagem de bits para 0 novamente
+    #        senão (ou seja, se a transimissão ainda não acabou):
+    #            o bit do quadro na posição igual ao contador de bits é igual ao bit presente no sinal "dados"
+    #            e soma um à contagem de bits
+    #    se o barramento estiver inativo e o sinal "dados" tiver valor 1 (ou seja, não comunicação no barramente e o escravo recebe o start bit do quadro):
+    #        muda o estado do barramento para ativo
+    #        e guarda o start bit na primeira posição do quadro (porque a contagem de bits está em zero).
+    #
+    @always(clock.posedge)
+    def leitura():
+        if not estado['barramento_inativo']:
+            if estado['contagem_bits'] == constantes['tamanho_quadro']:
+                estado['barramento_inativo'] = True
+                estado['contagem_bits'] = 0
+                
+                for x in range(126,0,-1):
+                    if quadro[x] != '1':
+                        quadro.pop(x)
+                    else:
+                        break
+
+                estado['SOF'] = quadro[0]
+                estado['ARBITRATION'] = quadro[1:13]
+                estado['CONTROL'] = quadro[13:19]
+
+                estado['DATA'] = quadro[19:-25]
+                estado['CRC'] = quadro[-25:-9]
+
+                estado['ACK'] = quadro[-9:-7]
+                estado['EOF'] = quadro[-7:]
+
+                print(estado)
+
+                #remove o encapsulamento da mensagem
+                msg = estado['DATA']+estado['CRC']
+
+                # Analisa a paridade -> # Retira o bit inserido
+                msg = paridadesOFF(''.join(str(e) for e in msg))
+
+                # Chave do CRC
+                key = "1100010110011001"
+
+                # Converte do binário de máquina para uma string
+                ans = decodeData(msg, key)
+                print("RESTANTE APÓS A DECODIFICAÇÃO: " + ans)
+                
+                # Realiza a verificação de erro - CRC
+
+                # Contabiliza quantos zeros precisa ter na resposta
+                temp = "0" * (len(key) - 1)
+                
+                # Se o ans for igual ao temp, printa a resposta verdadeira
+                if ans == temp:
+                    print("THANKS. DADOS: <SAIDA>" + msg + "</SAIDA> NENHUM ERRO ENCONTRADO")
+                    
+                    # Manda o resultado para o cliente
+                    print("OBRIGADA POR CONECTAR -> NENHUM ERRO ENCONTRADO")
+                
+                else:
+                    print("ERRO NOS DADOS")
+                    
+                    # Manda o resultado para o cliente
+                    print("OBRIGADA POR CONECTAR -> ERRO NOS DADOS")
+
+            else:
+                #print("DADOS ", estado['contagem_bits']," leitura", (dados))
+                quadro[estado['contagem_bits']] = str(dados)
+                estado['contagem_bits'] += 1
+        elif dados == 1:
+            print("Ativou o barramento")
+            estado['barramento_inativo'] = False
+
+            #quadro[estado['contagem_bits']] = str(dados)
+            #estado['contagem_bits'] += 1
+
+    # todo bloco sempre retorna as funções (e somente as funções) definidas dentro dele.
+    return leitura
+
+
+
+
+
+
+# Bloco Mestre: define estrutura do bloco que enviará o pacote para o escravo.
+# recebe dois sinais:
+# "clock": necessário para a simulaçao funcionar.
+# "dados": onde o mestre vai colocar os bits a serem enviados para o escravo.
+@block
+def Mestre(clock, dados):
+    constantes = {
+        'tamanho_quadro': 127
+    }
+
+    # o mestre manda no barramento, por isso ele pode assumir que o barramento está livre,
+    # uma vez que apenas ele escreve no barramento.
+    # então o único estado variável que ele precisa manter é quantos bits já foram enviados.
+    estado = {
+        'contagem_bits': 0
+    }
+    #Teste de envio! 
+    quadro = [1, 1, 0, 1, 1, 0, 1, 0, 0, 1]
+
+    #chamar o encode aqui
+    msg = input("DIGITE A MENSAGEM EM BINARIO QUE VOCÊ DESEJA ENVIAR: ")
+
+    print("MENSAGEM RECEBIDA:    ",msg)
+
+    # Define o CRC
+    key = "1100010110011001"
+
+    # encodeData = Codificação da mensagem
+    ans = encodeData(msg, key)
+
+    print("MENSAGEM COM CRC:    ",ans)
+
+    # Entra com o verificador de segurança!
+    ans = paridadesIN(ans)
+
+    print("MENSAGEM COM Paridade:    ",ans)
+
+
+    # Faz o encapsulamento da mensagem
+    
+    ativador="1" # Ativa o barramento manualmente...
+
+    encap = "0100110001000001000" #len = 19
+    encap2 = "011111111" #len = 9
+
+    ans = str(ativador) + str(encap) + ans + str(encap2)
+    
+    print("MENSAGEM ENCAPSULADA E ENVIADA:    ",ans)
+
+
+    quadro = list(map(int, str(ans)))
+
+    print("Tamanho da mensagem: ",len(quadro))
+
+    # função "escrita": é chamada a cada subida do clock;
+    # pseudocódigo de funcionamento:
+    #
+    #   se o contador de bits enviados for menor que o tamanho do quadro (ou seja, ainda há bits a enviar) então:
+    #       põe o próximo bit no sinal dados
+    #       e incremente contador de bits
+    #   senão (ou seja, se todos os bits já foram enviados):
+    #       coloca o barramento em nível inativo, que é o bit 0
+    #
+    @always(clock.posedge)
+    def escrita():
+        #TODO TAMANHO DO QUADRO VARIAVEL, NECESSIDADE DE ADAPTAÇÃO DO CODIGO
+        if estado['contagem_bits'] < constantes['tamanho_quadro'] and estado['contagem_bits'] < len(quadro):
+            #print("BIT ",estado['contagem_bits']," ENVIADO", quadro[estado['contagem_bits']])
+            dados.next = quadro[estado['contagem_bits']]
+            estado['contagem_bits'] += 1
+        else:
+            dados.next = 0
+
+    return escrita
+
+# Block Top: bloco que cria e conecta os nós.
+@block
+def Top():
+    clock_global = Signal(0) # criação do sinal de clock que vai atuar nos nós.
+    dados = Signal(0) # criação do sinal que representa o barramento.
+    dados2 = Signal(1)
+
+    escravo = Escravo(clock_global, dados) # criação de um escravo conectado ao clock e ao barramento.
+    escravo2 = Escravo(clock_global, dados2) # criação de um escravo conectado ao clock e ao barramento.
+    
+    mestre = Mestre(clock_global, dados) # ciração de um mestre conectado ao clock e ao barramento.
+    mestre2 = Mestre(clock_global, dados2) # ciração de um mestre conectado ao clock e ao barramento.
+
+    
+    #                                          __II_
+    #                                         /     \
+    #                                        /_______\
+    #                                        |  _  0 |
+    #                                        | | |   |
+    ################################################################################################
+    # DEVER DE CASA 4                                                                              #
+    ################################################################################################
+    # Crie um segundo escravo e simule um barramento com esses dois escravos. Utilize o mecanismo  #
+    # de endereçamento do seu protocolo (por exemplo, o ID na rede CAN) para diferenciar entre os  #
+    # escravos.                                                                                    #
+    ################################################################################################
+
+    # Função que gera sinal de clock alternando o nível lógico do sinal "clock_global" a cada 10ns.
+    @always(delay(10))
+    def clock():
+        clock_global.next = not clock_global
+
+    # Retorna funções criadas dentro do bloco Top.
+    return clock, escravo,escravo2, mestre, mestre2
+
+
+# Até agora, apenas estruturas de blocos foram definidas. Chegou a hora de criar instâncias e executá-las.
+# Abaixo, "inst" é uma instância do bloco Top.
+inst = Top()
+
+# Executar a simulação para 500ns.
+inst.run_sim(5000)
